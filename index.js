766edceb3ffec3adfa364be17dce4521d6b39713
Justin <156034244+JustinRucinski@users.noreply.github.com>
2024-02-27 00:37:39 +0000

    edited image 3 easing

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
     ScrollReveal().reveal('.header', {delay:50,});
     ScrollReveal().reveal('.image_1', {delay:500, distance: '0px', opacity: .20});
     ScrollReveal().reveal('.image_2', {delay:500, distance: '500px', origin: 'right', rotate: {x: 20, z: 20}});
-    ScrollReveal().reveal('.image_3', {delay:500, distance: '500px', origin: 'bottom', scale: .25});
+    ScrollReveal().reveal('.image_3', {delay:500, distance: '500px', origin: 'bottom', easing: 'ease-in'});
     ScrollReveal().reveal('.image_4', {delay:100, distance: '500px', origin: 'top', rotate: {x: 45, y: 45, z: 45}});
     ScrollReveal().reveal('.image_5', {delay:100, distance: '500px', orgin: 'bottom', rotate: {x: 90, y: 90, z: 90}});
  }
+
