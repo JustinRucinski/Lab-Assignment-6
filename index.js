4e55207b4ebde6b52930a9edeb3e32394c347c73
Justin <156034244+JustinRucinski@users.noreply.github.com>
2024-02-27 00:55:05 +0000

    fixed errors in JS

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
     ScrollReveal().reveal('.header', {delay:50,});
     ScrollReveal().reveal('.image_1', {delay:500, distance: '0px', opacity: .05, duration: 700});
     ScrollReveal().reveal('.image_2', {delay:500, distance: '500px', origin: 'right', easing: 'steps(5)', duration: 800});
-    ScrollReveal().reveal('.image_3', {delay:500, distance: '500px', origin: 'bottom', easing: 'ease-in'});
+    ScrollReveal().reveal('.image_3', {delay:500, distance: '500px', origin: 'left', easing: 'ease-in', duration: 900});
     ScrollReveal().reveal('.image_4', {delay:100, distance: '500px', origin: 'top', rotate: {x: 20, z: 20}});
-    ScrollReveal().reveal('.image_5', {delay:100, distance: '500px', orgin: 'bottom', rotate: {x: 90, y: 90, z: 90}});
+    ScrollReveal().reveal('.image_5', {delay:100, distance: '500px', origin: 'bottom', rotate: {x: 90, y: 90, z: 90}});
  }
 
