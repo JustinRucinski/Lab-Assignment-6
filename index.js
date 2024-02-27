820297c4bcb67c09f56142bf8f293880b273a52c
Justin <156034244+JustinRucinski@users.noreply.github.com>
2024-02-27 00:32:56 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Add JavaScript below
+ window.onload = function() {
+    ScrollReveal().reveal('.header', {delay:50,});
+    ScrollReveal().reveal('.image_1', {delay:500, distance: '0px', opacity: .20});
+    ScrollReveal().reveal('.image_2', {delay:500, distance: '500px', origin: 'right', rotate: {x: 20, z: 20}});
+    ScrollReveal().reveal('.image_3', {delay:500, distance: '500px', origin: 'bottom', scale: .25});
+    ScrollReveal().reveal('.image_4', {delay:500, distance: '500px', origin: 'top', rotate: {x: 45, y: 45, z: 45}});
+    ScrollReveal().reveal('.image_5', {delay:500, distance: '500px', orgin: 'bottom', rotate: {x: 90, y: 90, z: 90}});
+ }
