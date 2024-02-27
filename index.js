b9919a76829869aaf19be29b51c761a62ec71097
Justin <156034244+JustinRucinski@users.noreply.github.com>
2024-02-27 00:51:00 +0000

    adjusted reveals of 1, 2, 3, 4

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
  window.onload = function() {
     ScrollReveal().reveal('.header', {delay:50,});
-    ScrollReveal().reveal('.image_1', {delay:500, distance: '0px', opacity: .20});
-    ScrollReveal().reveal('.image_2', {delay:500, distance: '500px', origin: 'right', rotate: {x: 20, z: 20}});
+    ScrollReveal().reveal('.image_1', {delay:500, distance: '0px', opacity: .05, duration: 700});
+    ScrollReveal().reveal('.image_2', {delay:500, distance: '500px', origin: 'right', easing: 'steps(5)', duration: 800});
     ScrollReveal().reveal('.image_3', {delay:500, distance: '500px', origin: 'bottom', easing: 'ease-in'});
-    ScrollReveal().reveal('.image_4', {delay:100, distance: '500px', origin: 'top', scale: .50});
+    ScrollReveal().reveal('.image_4', {delay:100, distance: '500px', origin: 'top', rotate: {x: 20, z: 20}});
     ScrollReveal().reveal('.image_5', {delay:100, distance: '500px', orgin: 'bottom', rotate: {x: 90, y: 90, z: 90}});
  }
 
