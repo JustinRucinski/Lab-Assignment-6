f23453cf4538ba3f49c0a9d10896b8fa062cf119
Justin <156034244+JustinRucinski@users.noreply.github.com>
2024-02-27 00:41:10 +0000

    edited image 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
     ScrollReveal().reveal('.image_1', {delay:500, distance: '0px', opacity: .20});
     ScrollReveal().reveal('.image_2', {delay:500, distance: '500px', origin: 'right', rotate: {x: 20, z: 20}});
     ScrollReveal().reveal('.image_3', {delay:500, distance: '500px', origin: 'bottom', easing: 'ease-in'});
-    ScrollReveal().reveal('.image_4', {delay:100, distance: '500px', origin: 'top', rotate: {x: 45, y: 45, z: 45}});
+    ScrollReveal().reveal('.image_4', {delay:100, distance: '500px', origin: 'top', easing 'cubic-bezier(.5,0,0,1)'});
     ScrollReveal().reveal('.image_5', {delay:100, distance: '500px', orgin: 'bottom', rotate: {x: 90, y: 90, z: 90}});
  }
 
